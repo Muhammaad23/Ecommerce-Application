d1260d4f583d265d708620d2ce58cd9c9dfc927c
Simply Done <muhammadmominov2004@gmail.com>
2024-11-11 02:59:59 +0500

    Product list

--- Ecommerce Application/ecommerce/products/admin.py
+++ Ecommerce Application/ecommerce/products/admin.py
@@ -3,3 +3,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(QuantityVariant)
+admin.site.register(ColorVariant)
+admin.site.register(SizeVariant)
--- Ecommerce Application/ecommerce/products/models.py
+++ Ecommerce Application/ecommerce/products/models.py
@@ -15,6 +15,29 @@ class Category(models.Model):
         return self.category_name
 
 
+
+
+class QuantityVariant(models.Model):
+    variant_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.variant_name
+
+class ColorVariant(models.Model):
+    color_name = models.CharField(max_length=100)
+    color_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.color_name
+
+
+class SizeVariant(models.Model):
+    size_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.size_name
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=100)
@@ -22,6 +45,10 @@ class Product(models.Model):
     price = models.CharField(max_length=20)
     description = models.TextField()
     stock = models.IntegerField(default=100)
+
+    quantity_type = models.ForeignKey(QuantityVariant, blank=True, null=True, on_delete=models.PROTECT)
+    color_type = models.ForeignKey(ColorVariant, blank=True, null=True, on_delete=models.PROTECT)
+    size_type = models.ForeignKey(SizeVariant, blank=True, null=True, on_delete=models.PROTECT)
 
 
     def __str__(self):
